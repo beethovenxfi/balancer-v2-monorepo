e50446a3900688f83ab75c7c3f5fde805a32faa4
Daniel <danielBeets@protonmail.com>
2022-10-07 08:47:25 +0200

    use a more precise method of calculating required token to wrap to avoid rounding errors

--- pkg/pool-linear/contracts/reaper/ReaperLinearPoolRebalancer.sol
+++ pkg/pool-linear/contracts/reaper/ReaperLinearPoolRebalancer.sol
@@ -24,8 +24,6 @@ import "../LinearPoolRebalancer.sol";
 contract ReaperLinearPoolRebalancer is LinearPoolRebalancer {
     using Math for uint256;
 
-    uint256 private immutable _divisor;
-
     // These Rebalancers can only be deployed from a factory to work around a circular dependency: the Pool must know
     // the address of the Rebalancer in order to register it, and the Rebalancer must know the address of the Pool
     // during construction.
@@ -33,8 +31,6 @@ contract ReaperLinearPoolRebalancer is LinearPoolRebalancer {
         LinearPoolRebalancer(ILinearPool(ILastCreatedPoolFactory(msg.sender).getLastCreatedPool()), vault, queries)
     {
         IERC20 wrappedToken = ILinearPool(ILastCreatedPoolFactory(msg.sender).getLastCreatedPool()).getWrappedToken();
-
-        _divisor = 10**IReaperTokenVault(address(wrappedToken)).decimals();
     }
 
     function _wrapTokens(uint256 amount) internal override {
@@ -51,9 +47,11 @@ contract ReaperLinearPoolRebalancer is LinearPoolRebalancer {
     }
 
     function _getRequiredTokensToWrap(uint256 wrappedAmount) internal view override returns (uint256) {
+        IReaperTokenVault tokenVault = IReaperTokenVault(address(_wrappedToken));
+
         // We round up to ensure the returned value will always be enough to get `wrappedAmount` when unwrapping.
         // This might result in some dust being left in the Rebalancer.
-        // wrappedAmount * pricePerFullShare / 10^decimals
-        return wrappedAmount.mul(IReaperTokenVault(address(_wrappedToken)).getPricePerFullShare()).divUp(_divisor);
+        // wrappedAmount * vaultBalance / vaultTotalSupply
+        return wrappedAmount.mul(tokenVault.balance()).divUp(tokenVault.totalSupply());
     }
 }
